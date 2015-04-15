bfd8a4484a8ceb05feb888d50339be90794ac432
Will Stephenson <wstephenson@suse.de>
2015-04-15 14:41:58 +0200

    draw a static background

--- stc.js
+++ stc.js
@@ -1,10 +1,39 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'starport-traffic-control', { preload: preload, create: create, update: update });
 
+var sunColor = 0xffd900;
+var planetColor = 0x1ED47C;
+var backgroundColor = 0xb466cc;
+
+var graphics;
+
+var planets;
+
 function preload() {
 }
 
 function create() {
-  game.stage.backgroundColor = "#7127CC";
+  graphics = game.add.graphics(0, 0);
+
+  drawBackground();
+}
+
+function drawBackground() {
+  game.stage.backgroundColor = backgroundColor;
+  drawSun();
+  drawPlanet();
+}
+
+function drawSun() {
+  var radius = game.world.height / 2 ;
+  graphics.beginFill(sunColor);
+  graphics.drawEllipse(game.world.width + radius / 2, game.world.centerY, radius, radius);
+  graphics.endFill();
+}
+
+function drawPlanet() {
+  var radius = game.world.width / 12;
+  graphics.beginFill(planetColor);
+  graphics.drawEllipse(game.world.width / 8, game.world.centerY, radius, radius);
 }
 
 function update() {
